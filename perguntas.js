d375b382c214ecd628ce8bb54461c8b421f13e42
TiagogaiT <146733245+TiagogaiT@users.noreply.github.com>
2024-09-23 11:55:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Astronomia',
+    'Qual é o maior planeta do nosso Sistema Solar?',
+    'Júpiter.'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde fica Londres?',
+    'Capital do Reino Unido.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o valor de Pi (π)?',
+    '3,14159265...'
+)
+
+criaCartao(
+    'Inglês',
+    'Como que pronuncia carro em inglês?',
+    'Car.'
+)
+
+criaCartao(
+    'Português',
+    'O que é um verbo?',
+    'É a palavra que indica ação, estado ou fenômeno da natureza.'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é a fotossíntese?',
+    'É o processo em que as plantas produzem seu próprio alimento usando luz do sol, água e gás carbônico.'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quem foi Sócrates?',
+    'Foi um filósofo grego que acreditava que o conhecimento vinha de fazer perguntas e buscar respostas através do diálogo.'
+)
+
+criaCartao(
+    'História',
+    'Quem colonizou o Brasil?',
+    'Pedro Álvares Cabral.'
+)
+
+criaCartao(
+    'Espanhol',
+    'Como que pronuncia bom dia em espanhol?',
+    'Buenos días.'
+)
